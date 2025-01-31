867953e8035694bd505605aa358be33ba73ea278
Muhammad Abdul Azizul Hakim <113509874+azihakim@users.noreply.github.com>
2025-01-31 13:00:31 +0700

    ss

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -25,12 +25,12 @@
 							<div>
 								<h4 class="card-title">Data Pengguna</h4>
 							</div>
-							@if (Auth::user()->jabatan == 'Admin')
+							{{-- @if (Auth::user()->jabatan == 'Admin')
 								<div>
 									<a href="{{ route('user.create') }}" class="btn btn-outline-primary btn-icon-text">
 										<i class="fa fa-plus-square btn-icon-prepend"></i> Tambah Pengguna</a>
 								</div>
-							@endif
+							@endif --}}
 						</div>
 						<table id="example" class="display" style="width:100%">
 							<thead>
